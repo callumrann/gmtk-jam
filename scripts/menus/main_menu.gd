bbe826db190034b489f08c9a8ac108e002078b7e
callumrann <callumrann@gmail.com>
2026-07-23 16:08:37 +1000

    feat: add placeholder level select screen

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+func _on_continue_pressed() -> void:
+	SceneManager.show_scene("res://scenes/levels/levels_loader.tscn")
+
+func _on_level_select_pressed() -> void:
+	SceneManager.show_scene("res://scenes/menus/level_select.tscn")
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
